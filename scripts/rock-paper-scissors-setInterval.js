98be9b1405eb7a21b087dc33e5655b2b3ad3facc
molinoB <102395829+molinoBen@users.noreply.github.com>
2023-06-01 11:59:35 +0800

    Add event listener to rockPaperScissor game

--- scripts/rock-paper-scissors-setInterval.js
+++ scripts/rock-paper-scissors-setInterval.js
@@ -46,6 +46,36 @@ function autoPlay() {
   }
 }
 
+document.querySelector('.js-rock-button').addEventListener('click', () => {
+  playGame('Rock');
+});
+document.querySelector('.js-paper-button').addEventListener('click', () => {
+  playGame('Paper');
+});
+document.querySelector('.js-scissors-button').addEventListener('click', () => {
+  playGame('Scissors');
+});
+document.querySelector('.js-reset-button').addEventListener('click', () => {
+  score.wins = 0;
+  score.losses = 0;
+  score.ties = 0;
+  localStorage.removeItem('score');
+  updateScoreElement();
+});
+document.querySelector('.js-autoplay-button').addEventListener('click', () => {
+  autoPlay();
+});
+
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'r') {
+    playGame('Rock');
+  } else if (event.key === 'p') {
+    playGame('Paper');
+  } else if (event.key === 's') {
+    playGame('Scissors');
+  }
+});
+
 function playGame(playerMove) {
   const computer = pickComputer();
   let result = '';
